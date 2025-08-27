bad4ebe5c9ccb0c29b88cd6c5e3011a2a5dc93f6
jwah101 <hon_achernar@naver.com>
2025-08-27 09:48:34 +0900

    survey 시스템

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+
+import { Route, Routes} from 'react-router-dom'
 import './App.css'
+import Survey from './pages/Survey'
+
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <div>
+      <Routes>
+        <Route path='survey' element= {<Survey />}></Route>
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/pages/Survey.jsx
@@ -0,0 +1,12 @@
+import Question from "../components/Question";
+import './Survey.css'
+const Survey = () =>{
+
+  return(
+    <div className="Survey-container">
+      <Question />
+    </div>
+  )
+}
+
+export default Survey;
