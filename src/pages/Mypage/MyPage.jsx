4ea83102d4936b94292edcefcc226d24b120ecec
jungstinbieber <suhunjeong10@gmail.com>
2025-08-28 15:41:05 +0900

    Feat:마이페이지프론트구현

--- src/pages/Mypage/MyPage.jsx
+++ src/pages/Mypage/MyPage.jsx
@@ -1,41 +1,37 @@
 import "./MyPage.css";
 
-function MyPage(){
-    return(
-        <>
-        <section className="login-form-container">
-<h1 className="form-title"> 👨‍👦 마이페이지 </h1>
+function MyPage() {
+  return (
+    <>
+      <section className="mypage-form-container">
+        <h1 className="mypage-form-title">👨‍👦 마이페이지</h1>
 
-<div className="input-group">
-<label htmlFor='username'>아이디 : </label>
-<input id='username' type='text' name='username'placeholder='' required/> <br />
-</div>
+        <div className="mypage-input-group">
+          <label htmlFor="mypage-username">아이디 :</label>
+          <input id="mypage-username" type="text" name="username" placeholder="" required /> <br />
+        </div>
 
-<div className="input-group">
-<label htmlFor='password'>비밀번호 : </label>
-<input id="password" type='text' name='password' placeholder='' required/> <br />
-</div>
+        <div className="mypage-input-group">
+          <label htmlFor="mypage-password">비밀번호 :</label>
+          <input id="mypage-password" type="text" name="password" placeholder="" required /> <br />
+        </div>
 
-<div className="input-group">
-<label htmlFor='password'>email : </label>
-<input id="password" type='text' name='password' placeholder='' required/> <br />
-</div>
+        <div className="mypage-input-group">
+          <label htmlFor="mypage-email">email :</label>
+          <input id="mypage-email" type="text" name="email" placeholder="" required /> <br />
+        </div>
 
-<div className='group-button'>
-<button>수정</button>
+        <div className="mypage-group-button">
+          <button>수정</button>
+          <button>탈퇴</button>
+        </div>
 
-<button>탈퇴</button>
-</div>
-
-
-
-<div className='to-Signup-page'>
-  <button>HOME 이동하기</button>
-</div>
-
-</section>
-        </>
-    )
+        <div className="mypage-to-home-page">
+          <button>HOME 이동하기</button>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default MyPage;
