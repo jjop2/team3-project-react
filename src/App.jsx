682c891642cf8193cb307187d135d6bdb55d304d
jwah101 <hon_achernar@naver.com>
2025-09-01 10:56:13 +0900

    Hotfix : 장르 추출시 preferGenre 값 수정

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,14 @@ function App() {
   const [auth, setAuth] = useState();
   const [userInfo, setUserInfo] = useState();
   const [topTwoGenres , setTopTwoGenres] = useState([]);
-  const [selectGenre , setSelectGenre] = useState("");
   const [surveyResultInfo, setSurveyResultInfo] = useState({
     age: "",
     gender: "",
     preferGenre1: "",
     preferGenre2: "",
   })
-
+  console.log(surveyResultInfo)
+ 
   useEffect(() => {
     if(sessionStorage.getItem('jwt') != null)
       setAuth('true');
@@ -72,8 +72,8 @@ function App() {
           <Route path="/login" element={<Login setAuth={setAuth} />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/mypage" element={<MyPage />}></Route>
-          <Route path="/survey" element={<Survey setTopTwoGenres={setTopTwoGenres} selectGenre={selectGenre} setSurveyResultInfo={setSurveyResultInfo}/>}/>
-          <Route path="/surveyresult" element={<SurveyResult topTwoGenres={topTwoGenres} setSelectGenre={setSelectGenre} surveyResultInfo={surveyResultInfo}/>} />
+          <Route path="/survey" element={<Survey setTopTwoGenres={setTopTwoGenres} setSurveyResultInfo={setSurveyResultInfo}/>}/>
+          <Route path="/surveyresult" element={<SurveyResult topTwoGenres={topTwoGenres} surveyResultInfo={surveyResultInfo}/>} />
         </Routes>
       </main>
 
