73293507801d9a62c3588f9ab1e43c7103a920d2
jwah101 <hon_achernar@naver.com>
2025-09-01 12:13:03 +0900

    hoxfix : mypage 수정

--- src/components/TabContents.jsx
+++ src/components/TabContents.jsx
@@ -1,6 +1,23 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+import axiosInstance from "../axiosInstance";
 
 const TabContents = ({tabNumber}) => {
+
+  const[genre, setGenre] = useState("");
+
+
+  useEffect(()=>{
+     axiosInstance.get(`${import.meta.env.VITE_SERVER_URL}/surveyresult`)
+      .then(response =>{
+        console.log(response.data)
+        setGenre(response.data.combinationGenre)
+       }).catch(error =>{
+        console.error(error);
+      })
+  },[])
+
+
   return(
     <>
     {
@@ -8,13 +25,7 @@ const TabContents = ({tabNumber}) => {
         <div>
           <h2>나의 선호 장르</h2>
           <p>
-            {axiosinstance.get(`${import.meta.env.VITE_SERVER_URL}/surveyresult`)
-            .then(response =>{
-              console.log(response.data)
-            }).catch(error =>{
-              console.error(error);
-            })
-            }
+           <img src={`/images/${genre}.PNG`} alt={genre} style={{width:"400px", height:"100%"}}/>
           </p>
         </div>,
         <div>
