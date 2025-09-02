e4e912dbdfd1af33396b3595eaa8bbc8a74158c9
jjop2 <jngsuh616@gmail.com>
2025-09-02 17:19:58 +0900

    Feat: 게시판 기능 구현 - 백엔드 확인용 커밋

--- src/pages/Board/Board.jsx
+++ src/pages/Board/Board.jsx
@@ -4,42 +4,40 @@ import './Board.css'
 import { useEffect, useState } from "react";
 import axiosInstance from "../../axiosInstance";
 
-import sampleImg from "../../images/모험돌이.png"
-
 const Board = () => {
   const {goTo} = useNavi();
   const [list, setList] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   axiosInstance.get('/board')
-  //     .then(response => setList(response.data))
-  //     .catch(error => console.error(error))
-  //     .finally(() => setLoading(false));
-  // }, [])
+  useEffect(() => {
+    axiosInstance.get('/board')
+      .then(response => setList(response.data))
+      .catch(error => console.error(error))
+      .finally(() => setLoading(false));
+  }, [])
 
-  // if(loading)
-  //   return <div>게시글 목록 불러오는 중...</div>
-
-
-  // if(!list)
-  //   return <div>등록된 게시물이 없습니다.</div>
+  if(loading)
+    return <div>게시글 목록 불러오는 중...</div>
 
 
-  // // 게시글 카드
-  // function boardCard(board) {
-  //   return (
-  //     <div className="boardCard" onClick={() => {
-  //       goTo(`/board/${board.id}`);
-  //     }}>
-  //       <div className="boardImg" style={{'backgroundImage':`url(${import.meta.env.VITE_SERVER_URL}/upload/${board.img})`}}></div>
-  //       <div className="boardText">
-  //         <h3>{board.title}</h3>
-  //         <p>{board.writer}</p>
-  //       </div>
-  //     </div>
-  //   )
-  // }
+  if(!list)
+    return <div>등록된 게시물이 없습니다.</div>
+
+
+  // 게시글 카드
+  function boardCard(board) {
+    return (
+      <div className="boardCard" onClick={() => {
+        goTo(`/board/${board.id}`);
+      }}>
+        <div className="boardImg" style={{'backgroundImage':`url(${import.meta.env.VITE_SERVER_URL}/upload/${board.img})`}}></div>
+        <div className="boardText">
+          <h3>{board.title}</h3>
+          <p>{board.writer}</p>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
@@ -48,7 +46,7 @@ const Board = () => {
         <button onClick={() => goTo('/board/write')}>게시글 작성</button>
 
         <div className="boardList">
-          {/* {
+          {
             list.map((board, i) => {
               return (
                 <div key={i}>
@@ -56,21 +54,7 @@ const Board = () => {
                 </div>
               )
             })
-          } */}
-          
-          {Array.from({ length: 10 }).map((_, index) => (
-            <div className="boardCard" key={index}>
-              <div
-                className="boardImg"
-                style={{ backgroundImage: `url(${sampleImg})` }}
-              ></div>
-              <div className="boardText">
-                <h3>제목 {index + 1}</h3>
-                <p>작성자</p>
-              </div>
-            </div>
-          ))}
-
+          }
 
         </div>
       </div>
